28a1fb8ee9a5d51d252ade411e7d179843ed91b5
Dani <roehtli@me.com>
2015-01-27 11:16:49 -0800

    Gui Klasse erstellt

---
+++ vokab/src/ch/zbw/vokab1316b/java/Vokab_GUI.java
@@ -0,0 +1,43 @@
+package ch.zbw.vokab1316b.java;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Vokab_GUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Vokab_GUI window = new Vokab_GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Vokab_GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
