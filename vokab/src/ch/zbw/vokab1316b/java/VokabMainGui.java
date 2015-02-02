a4c6af744fb9da81b33fb91b2e788c097cbfce78
bama14 <marcelb@gmx.ch>
2015-02-02 17:01:56 +0100

    GUI Test

--- vokab/src/ch/zbw/vokab1316b/java/VokabMainGui.java
+++ vokab/src/ch/zbw/vokab1316b/java/VokabMainGui.java
@@ -3,7 +3,7 @@ package ch.zbw.vokab1316b.java;
 public class VokabMainGui {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub test
 
 	}
 
