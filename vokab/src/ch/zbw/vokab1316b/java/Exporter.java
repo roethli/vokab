2686de178dac8ad7fba5a30a19cc4da62ac9ecde
hapieep <hapieep@gmail.com>
2015-02-08 18:24:24 +0100

    Dummy-Methoden hinzugefügt zu Importer / Exporter

--- vokab/src/ch/zbw/vokab1316b/java/Exporter.java
+++ vokab/src/ch/zbw/vokab1316b/java/Exporter.java
@@ -1,5 +1,12 @@
 package ch.zbw.vokab1316b.java;
 
+import java.util.ArrayList;
+import ch.zbw.vokab1316b.java.Vocabulary;
+
 public class Exporter {
+	
+	public void exportToFile(ArrayList<Vocabulary> arrList) {
+		
+	}
 
 }
