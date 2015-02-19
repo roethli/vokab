43dc14c13bb0be0d611ea70284b3b6b1e7452ad7
hapieep <hapieep@gmail.com>
2015-02-19 20:21:03 +0100

    Javadoc-Kommentare und weitere Kommentare hinzugefügt

--- vokab/src/ch/zbw/vokab1316b/java/Exporter.java
+++ vokab/src/ch/zbw/vokab1316b/java/Exporter.java
@@ -2,16 +2,28 @@ package ch.zbw.vokab1316b.java;
 
 import java.util.ArrayList;
 import java.io.File;
-import ch.zbw.vokab1316b.java.Vocabulary;
 import ch.zbw.vokab1316b.java.VokabList;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
+/**
+ * This class implements exporting Vocubalary-Lists stored in an arraylist to a file encoded and serialized as XML.
+ * For marshalling JAXB is used. 
+ * 
+ * @author Habib Ben Abdelbaki, ZbW
+ * @version 1.0 19.02.2015
+ */
+
 public class Exporter {
+	/**
+	 * Exports Vocabulary objects to a file
+	 * 
+	 * @param file instance of <a href="http://docs.oracle.com/javase/7/docs/api/java/io/File.html">File</a> 
+	 * @param arraylist containing Vocabulary objects
+	 */
 
 	public void exportToFile(ArrayList<Vocabulary> arrList, File file) {
-		Vocabulary voc = new Vocabulary("Book", "Buch", 3);
 
 		try {
 			JAXBContext jaxbContext = JAXBContext.newInstance(VokabList.class);
--- vokab/src/ch/zbw/vokab1316b/java/Importer.java
+++ vokab/src/ch/zbw/vokab1316b/java/Importer.java
@@ -8,8 +8,21 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
-public class Importer {
+/**
+ * This class implements importing Vocubalary-Lists as arraylist from a file containing marshalled Vocaulary objects encoded in XML.
+ * For unmarshalling JAXB is used. 
+ * 
+ * @author Habib Ben Abdelbaki, ZbW
+ * @version 1.0 19.02.2015
+ */
 
+public class Importer {
+	/**
+	 * Imports Vocabulary objects from a file
+	 * 
+	 * @param file instance of <a href="http://docs.oracle.com/javase/7/docs/api/java/io/File.html">File</a> 
+	 * @return arraylist containing Vocabulary objects
+	 */
 	public ArrayList<Vocabulary> importFromFile(File file) {
 		VokabList vl = new VokabList();
 		try {
